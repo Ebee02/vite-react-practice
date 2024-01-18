d7106dd0c450058f6ea0196f67f2cb7c73ab9d04
Ebee02 <beelawrence8@gmail.com>
2024-01-18 13:07:46 +0000

    update

--- src/components/MemeGeneratorApp/SecondForm.jsx
+++ src/components/MemeGeneratorApp/SecondForm.jsx
@@ -1,31 +1,49 @@
+import React, { useState } from "react";
+
 function SecondForm() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  console.log(formData);
+
+  function handleChange(e) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
   return (
-    <form
-      action="#"
-      className="antialiased font-semibold text-slate-800 flex flex-col max-w-sm w-full p-4"
-    >
-      <label htmlFor="first-name" className="text-xl  mb-2">
-        First Name:
-      </label>
-      <input
-        type="text"
-        id="first-name"
-        className="border border-black p-3 text-xl font-medium rounded-lg shadow-md shadow-gray-300 mb-4"
-      />
-      <label htmlFor="last-name" className="text-xl  mb-2">
-        Last Name:
-      </label>
-      <input
-        type="text"
-        id="last-name"
-        className="border border-black p-3 text-xl font-medium rounded-lg shadow-md shadow-gray-300 mb-4"
-      />
-      <input
-        type="submit"
-        value="Submit"
-        className="bg-gray-500 text-white w-32 py-4 rounded-md shadow-gray-300 shadow-md cursor-pointer transition-all duration-300 hover:bg-black hover:tracking-wider"
-      />
-    </form>
+    <div className="h-screen flex items-center justify-center">
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          className="border border-black py-2 px-3 rounded-md shadow-md text-lg w-80 tracking-wide"
+          placeholder="First Name"
+          onChange={handleChange}
+          name="firstName"
+        />
+        <input
+          type="text"
+          className="border border-black py-2 px-3 rounded-md shadow-md text-lg w-80 tracking-wide"
+          placeholder="Last Name"
+          onChange={handleChange}
+          name="lastName"
+        />
+        <input
+          type="email"
+          className="border border-black py-2 px-3 rounded-md shadow-md text-lg w-80 tracking-wide"
+          placeholder="Email"
+          onChange={handleChange}
+          name="email"
+        />
+      </form>
+    </div>
   );
 }
+
 export default SecondForm;

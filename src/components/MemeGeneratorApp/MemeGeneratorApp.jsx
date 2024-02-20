1d1af527ea9c5bb50e904f01feabe393406f205b
Ebee02 <beelawrence8@gmail.com>
2024-02-20 12:42:09 +0000

    update

--- src/components/MemeGeneratorApp/MemeGeneratorApp.jsx
+++ src/components/MemeGeneratorApp/MemeGeneratorApp.jsx
@@ -1,13 +1,15 @@
 import Header from "./Header";
 import Form from "./Form";
+import ApiPractice from "./ApiPractice";
 
 import "./style.css";
 
 function MemeGeneratorApp() {
   return (
     <>
-      <Header />
-      <Form />
+      {/* <Header />
+      <Form /> */}
+      <ApiPractice />
     </>
   );
 }
